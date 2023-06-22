fb58b1edf91a4007048293c0480ae75547746935
haider-patanwala <haiderpatanwala@hotmail.com>
2023-06-22 19:22:26 +0530

    Removed Search and Social Icons

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require("express")
-const fs = require("fs/promises")
-const utils = require("./utils/utils")
-const todoRouter = require("./routes/todos.routes")
-const authRouter = require("./routes/auth.routes")
-const viewsRouter = require("./routes/views.routes")
-const middlewares = require("./middlewares/index")
+const express = require("express");
+const fs = require("fs/promises");
+const utils = require("./utils/utils");
+const todoRouter = require("./routes/todos.routes");
+const authRouter = require("./routes/auth.routes");
+const viewsRouter = require("./routes/views.routes");
+const middlewares = require("./middlewares/index");
 
 // initialize the express app
-const app = express()
+const app = express();
 
 // set view engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // middlewares
-app.use(middlewares.logger)
-app.use(express.json())
+app.use(middlewares.logger);
+app.use(express.json());
 
 // hello world API call for our server/app
 app.get("/greetings", (req, res) => {
-	return res.send("Greetings from Todo App.")
-})
+	return res.send("Greetings from Todo App.");
+});
 
 // view routers
-app.use("/", viewsRouter)
+app.use("/", viewsRouter);
 
 // api routers
-app.use("/api/todos", todoRouter)
-app.use("/api/auth", authRouter)
+app.use("/api/todos", todoRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
-	console.log("server is running on port 3000")
-})
+	console.log("server is running on port 3000, http://localhost:3000");
+});
